b19295c1d8a712503aae2e6ae50b77d2404efc57
Ben Hanzl <benhanzl@honeycomb.io>
2023-07-17 11:44:12 -0400

    Added GET /products/:id API endpoint

--- README.md
+++ README.md
@@ -11,7 +11,12 @@ npm run start
 
 ## API
 
-`POST /api/products` - Returns an array of all products.
+`GET /api/products` - Returns an array of all products.
 
 - Response Code: 200 - Array of products
 - Returns an empty array if no products exist.
+
+`GET /api/products/:id` - Returns the product with the specified id.
+
+- Response Code: 200 - Individual product.
+- Response Code: 404 - Product does not exist.
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -7,4 +7,18 @@ productsRouter.get("/", async (_, response) => {
   response.json(products);
 });
 
+productsRouter.get("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const product = await db.product.findUnique({
+    where: { id },
+  });
+
+  if (!product) {
+    return response.status(404).send("Not Found");
+  }
+
+  response.json(product);
+});
+
 module.exports = productsRouter;
