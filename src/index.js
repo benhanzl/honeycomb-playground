0709e4115ba1d0436de03ba59ed32f5bf27e109e
Ben Hanzl <benhanzl@honeycomb.io>
2023-07-17 14:09:02 -0400

    Refactored logger into a module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
 const app = require("./app");
+const logger = require("./utils/logger");
 const productsRouter = require("./controllers/products");
 
 app.use("/api/products", productsRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
