1d7cf25850f3945343e954cc6666fe1cef02e54e
Ben Hanzl <benhanzl@honeycomb.io>
2023-07-17 11:24:46 -0400

    Configured Prisma and created Product model

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,10 @@
 const productsRouter = require("express").Router();
+const db = require("../utils/db");
 
-productsRouter.get("/", (_, response) => {
-  response.json([]);
+productsRouter.get("/", async (_, response) => {
+  const products = await db.product.findMany();
+
+  response.json(products);
 });
 
 module.exports = productsRouter;
